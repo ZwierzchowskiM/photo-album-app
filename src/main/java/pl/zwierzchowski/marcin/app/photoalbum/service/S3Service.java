c3a383ed54dfc97e127f4c8af7bc67981f9c1f06
ZwierzchowskiM <programowanie.mzwierzchowski@gmail.com>
2023-05-04 12:42:21 +0200

    add S3Service class

---
+++ src/main/java/pl/zwierzchowski/marcin/app/photoalbum/service/S3Service.java
@@ -0,0 +1,152 @@
+package pl.zwierzchowski.marcin.app.photoalbum.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import java.io.*;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    S3Client s3;
+
+    @Value("${accessKey}")
+    private String accessKey;
+    @Value("${secret}")
+    private String secret;
+
+    @Value("${bucketName}")
+    private String bucketName;
+
+
+    // Create the S3Client object.
+    private S3Client getClient() {
+
+        Region region = Region.EU_NORTH_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secret)))
+                .region(region)
+                .build();
+
+        return s3;
+    }
+
+    public String putObject(MultipartFile file) {
+
+        s3 = getClient();
+
+        isFileEmpty(file);
+
+        String objectKey = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+        try {
+            byte[] data = file.getBytes();
+
+            s3.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(objectKey)
+                            .build(),
+                    RequestBody.fromBytes(data));
+            return objectKey;
+
+        } catch (S3Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return null;
+    }
+
+    public byte[] getObjectBytes(String keyName) {
+
+        S3Client s3 = getClient();
+        try {
+            GetObjectRequest objectRequest = GetObjectRequest
+                    .builder()
+                    .key(keyName)
+                    .bucket(bucketName)
+                    .build();
+
+            ResponseBytes<GetObjectResponse> objectBytes = s3.getObjectAsBytes(objectRequest);
+            byte[] data = objectBytes.asByteArray();
+            return data;
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public void listBucketObjects() {
+        S3Client s3 = getClient();
+        try {
+            ListObjectsRequest listObjects = ListObjectsRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build();
+
+            ListObjectsResponse res = s3.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            for (S3Object myValue : objects) {
+                System.out.print("\n The name of the key is " + myValue.key());
+                System.out.print("\n The owner is " + myValue.owner());
+            }
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+
+
+    public void createBucket(String bucketName) {
+
+        S3Client s3Client = getClient();
+        try {
+            s3Client.createBucket(CreateBucketRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .build());
+            System.out.println("Creating bucket: " + bucketName);
+            s3Client.waiter().waitUntilBucketExists(HeadBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build());
+            System.out.println(bucketName + " is ready.");
+            System.out.printf("%n");
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+
+    private void isImage(MultipartFile file) {
+
+        String contentType = file.getContentType();
+        if (!(contentType.equals("image/png") || contentType.equals("image/jpg") || contentType.equals("image/jpeg"))) {
+            throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+        }
+    }
+
+}
