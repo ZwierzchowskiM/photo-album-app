2fdd68176a2fdc3a5b9b6aef6d886cc15a1ffa03
ZwierzchowskiM <programowanie.mzwierzchowski@gmail.com>
2023-05-08 23:56:34 +0200

    add photoController method, api.path

--- src/main/java/pl/zwierzchowski/marcin/app/photoalbum/web/PhotoController.java
+++ src/main/java/pl/zwierzchowski/marcin/app/photoalbum/web/PhotoController.java
@@ -1,4 +1,37 @@
 package pl.zwierzchowski.marcin.app.photoalbum.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import pl.zwierzchowski.marcin.app.photoalbum.repository.entity.PhotoEntity;
+import pl.zwierzchowski.marcin.app.photoalbum.service.PhotoService;
+import pl.zwierzchowski.marcin.app.photoalbum.service.S3Service;
+
+@RestController
+@RequestMapping("${api.path}/photos")
 public class PhotoController {
+
+    S3Service s3Service;
+    PhotoService photoService;
+
+    public PhotoController(S3Service s3Service, PhotoService photoService) {
+        this.s3Service = s3Service;
+        this.photoService = photoService;
+    }
+
+    @PostMapping("/save")
+    public PhotoEntity createPhoto(@RequestParam("file") MultipartFile file, String description) {
+
+        String S3address = "";
+        S3address = s3Service.putObject(file);
+        System.out.println("You have placed file into the S3 bucket");
+
+        PhotoEntity savedPhoto = photoService.save(S3address, description);
+
+        return savedPhoto;
+    }
+
+
 }
