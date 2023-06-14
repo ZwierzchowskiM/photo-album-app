b43cdd447dac10afb0c3aee69617db74076c0742
ZwierzchowskiM <programowanie.mzwierzchowski@gmail.com>
2023-06-14 14:42:09 +0200

    add UserMapper methods

--- src/main/java/pl/zwierzchowski/marcin/app/photoalbum/service/mapper/UserMapper.java
+++ src/main/java/pl/zwierzchowski/marcin/app/photoalbum/service/mapper/UserMapper.java
@@ -1,4 +1,22 @@
 package pl.zwierzchowski.marcin.app.photoalbum.service.mapper;
 
+import org.modelmapper.ModelMapper;
+import pl.zwierzchowski.marcin.app.photoalbum.repository.entity.UserEntity;
+import pl.zwierzchowski.marcin.app.photoalbum.web.model.UserModel;
+
 public class UserMapper {
+
+    public UserModel from(UserEntity userEntity) {
+        ModelMapper modelMapper = new ModelMapper();
+        UserModel userModel = modelMapper.map(userEntity, UserModel.class);
+        return userModel;
+    }
+
+    public UserEntity from(UserModel userModel) {
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity userEntity = modelMapper.map(userModel, UserEntity.class);
+        return userEntity;
+    }
+
+
 }
