33c72038c201f0cc0f88df16d7251988da3f0240
ZwierzchowskiM <marcin.zwierzchowski1@gmail.com>
2023-11-23 23:46:01 +0100

    add AlbumModel and AlbumMapper

---
+++ src/main/java/pl/zwierzchowski/marcin/app/photoalbum/web/model/AlbumModel.java
@@ -0,0 +1,14 @@
+package pl.zwierzchowski.marcin.app.photoalbum.web.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlbumModel {
+
+    private String albumTitle;
+    private String albumId;
+    private String url;
+}
